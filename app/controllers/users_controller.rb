ea7244305fce4eff058c9f7b9f2667506e7d632e
Alan <alanoliveiran45@gmail.com>
2017-03-13 17:59:34 -0300

    Edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	before_action :logged_in_user, only: [:edit, :update]
+
 	def new
 		@user = User.new
 	end
@@ -7,9 +9,13 @@ class UsersController < ApplicationController
 		@user = User.find params[:id]
 	end
 
+	def edit
+		@user = current_user
+	end
+
 	def create
 		@user = User.new user_params
-		if params[:user][:password] == params[:user][:confirm_password] 
+		if params[:user][:password] == params[:user][:confirm_password]
 			if @user.save
 				log_in @user
 				redirect_to @user
@@ -20,6 +26,28 @@ class UsersController < ApplicationController
 		else
 			flash[:error] = "As senhas não coincidem"
 			render :new
+		end
+	end
+
+	def update
+		if logged_in?
+			@user = current_user
+			if @user.update user_params
+				redirect_to @user
+			else
+				flash[:error] = "Não foi possível atualizar cadastro"
+				render :edit
+			end
+		else
+			redirect_to root_path
+		end
+	end
+
+
+	def logged_in_user
+		unless logged_in?
+			flash[:danger] = "Please log in."
+			redirect_to login_url
 		end
 	end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :cuisines, only:[:new, :create, :show]
   resources :food_types, only:[:new, :create, :show]
   resources :recipes, only:[:new, :create, :show, :index, :edit, :update]
-  resources :users, only:[:new, :create, :show]
+  resources :users, only:[:new, :create, :show, :edit, :update]
 
   post '/recipes/search', to: 'recipes#search'
 
